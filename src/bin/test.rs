cde1c1a7569bdb9f602a5696622c0def759ba528
Danny Willems <be.danny.willems@gmail.com>
2024-10-23 14:33:08 +0200

    Add doc link reg panic handler

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,6 +3,7 @@
 
 use core::panic::PanicInfo;
 
+// See https://doc.rust-lang.org/nomicon/panic-handler.html
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     // Note that this should include some o1vm infos
