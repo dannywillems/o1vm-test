e5a6f0eb38f9f95b3c0cc780c1ed62f09275ba5a
Danny Willems <be.danny.willems@gmail.com>
2024-10-23 14:32:41 +0200

    Remove useless comment

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -6,7 +6,6 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     // Note that this should include some o1vm infos
-    // logs "panicked at '$reason', src/main.rs:27:4" to the host stderr
     loop {}
 }
 
